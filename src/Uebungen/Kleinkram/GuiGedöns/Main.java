3f6459f24d7cd50b05348c252b003417805ff543
unknown <lnerlich@t8y-nerlichl.de.ddb.com>
2015-02-16 16:49:31 +0100

    gui uebungen

--- src/Uebungen/Kleinkram/GuiGedöns/Main.java
+++ src/Uebungen/Kleinkram/GuiGedöns/Main.java
@@ -4,4 +4,7 @@ package Uebungen.Kleinkram.GuiGedöns;
  * Created by lnerlich on 16.02.2015.
  */
 public class Main {
+    // Tastaturinput testen
+    // objekt mit maus steuern, ball bewegt sich paralelle zur maus
+    // eventuell auf zeit durch nen Labyrinth
 }
